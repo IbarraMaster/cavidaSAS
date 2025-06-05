a0c86697f61e167c3799c36cc403d23c52d1d1ae
IbarraMaster <ibarrahormaza18@gmail.com>
2025-06-05 09:42:52 -0500

    Se agrega los favicons a todas las paginas de la carpeta pages

---
+++ php/favicons.php
@@ -0,0 +1,10 @@
+<?php
+$rutaBase = '/imgs/favicon/';
+?>
+<head>
+  <link rel="icon" type="image/x-icon" href="<?php echo $rutaBase; ?>favicon.ico">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $rutaBase; ?>favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $rutaBase; ?>favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $rutaBase; ?>apple-touch-icon.png">
+  <link rel="manifest" href="<?php echo $rutaBase; ?>site.webmanifest">
+</head>
